c7eb36d90acbce73ca2b0b2f24ed54414f950f89
Enrico Shippole <henryshippole@gmail.com>
2022-07-09 18:11:02 -0400

    Add files via upload

--- cvt_flax/cvt_flax.py
+++ cvt_flax/cvt_flax.py
@@ -4,40 +4,32 @@ from jax.numpy import einsum
 
 import flax.linen as nn
 
+from typing import Callable
+
 from einops import rearrange
-
-def group_dict_by_key(cond, d):
-    return_val = [dict(), dict()]
-    for key in d.keys():
-        match = bool(cond(key))
-        ind = int(not match)
-        return_val[ind][key] = d[key]
-    return (*return_val,)
-
-def group_by_key_prefix_and_remove_prefix(prefix, d):
-    kwargs_with_prefix, kwargs = group_dict_by_key(lambda x: x.startswith(prefix), d)
-    kwargs_without_prefix = dict(map(lambda x: (x[0][len(prefix):], x[1]), tuple(kwargs_with_prefix.items())))
-    return kwargs_without_prefix, kwargs
 
 class LayerNorm(nn.Module): # layernorm, but done in the channel dimension #1
     dim: int
     eps: float = 1e-5
 
     @nn.compact
-    def call(self, x):
+    def __call__(self, x):
         eps = self.eps
+        g = self.param('g', nn.initializers.ones, [1, 1, 1, self.dim])
+        b = self.param('b', nn.initializers.zeros, [1, 1, 1, self.dim])
 
-        self.g = tf.Variable(tf.ones([1, 1, 1, dim]))
-        pos_embedding = self.param('pos_embedding', nn.initializers.zeros, [1, num_patches + 1, self.dim])
-        self.b = tf.Variable(tf.zeros([1, 1, 1, dim]))
-        pos_embedding = self.param('pos_embedding', nn.initializers.zeros, [1, num_patches + 1, self.dim])
+        var = jnp.var(x, axis = -1, keepdims = True)
+        mean = jnp.mean(x, axis = -1, keepdims = True)
 
-        var = tf.math.reduce_variance(x, axis=-1, keepdims=True)
-        mean = tf.reduce_mean(x, axis=-1, keepdims=True)
-
-        x = (x - mean) / tf.sqrt((var + eps)) * self.g + self.b
+        x = (x - mean) / jnp.sqrt((var + eps)) * g + b
 
         return x
+
+class GlobalAvgPool(nn.Module):
+
+    @nn.compact
+    def __call__(self, x):
+        return jnp.mean(x, axis = (1, 2))
 
 class PreNorm(nn.Module):
     fn: Callable
@@ -50,7 +42,7 @@ class PreNorm(nn.Module):
 class FeedForward(nn.Module):
     dim: int
     mult: int
-    dropout: float = 0.
+    dropout: float = 0.0
 
     @nn.compact
     def __call__(self, x):
@@ -63,38 +55,43 @@ class FeedForward(nn.Module):
 
 
 class DepthWiseConv2d(nn.Module):
-    dim_in 
-    dim_out 
-    kernel_size 
-    stride 
+    dim_in: int 
+    dim_out: int 
+    kernel_size: int
+    stride: int 
     bias: bool = True
 
     @nn.compact
     def __call__(self, x):
-        net = []
-        net += [nn.Conv(features = self.dim_in, kernel_size = self.kernel_size, strides = self.stride, padding = 'SAME', groups = self.dim_in, use_bias = self.bias)]
-        net += [nn.BatchNorm(use_running_average = False, momentum = 0.9, epsilon = 1e-5)]
-        net += [nn.Conv(features = self.dim_out, kernel_size = (1, 1), strides = (1, 1), use_bias = self.bias)] 
-        x = nn.Sequential(net)(x)
+        x = nn.Conv(
+            features = self.dim_in, 
+            kernel_size = (self.kernel_size, self.kernel_size), 
+            strides = (self.stride, self.stride), 
+            padding = 'SAME', 
+            feature_group_count = self.dim_in, 
+            use_bias = self.bias)(x)
+        x = nn.BatchNorm(use_running_average = False, momentum = 0.9, epsilon = 1e-5)(x)
+        x = nn.Conv(features = self.dim_out, kernel_size = (1, 1), strides = (1, 1), use_bias = self.bias)(x)
         return x
 
 class Attention(nn.Module): 
     dim: int 
-    proj_kernel 
-    kv_proj_stride 
+    proj_kernel: int 
+    kv_proj_stride: int 
     heads: int = 8 
     dim_head: int = 64 
     dropout: float = 0.
 
     @nn.compact
-    def call(self, x):
+    def __call__(self, x):
         inner_dim = self.dim_head * self.heads
         heads = self.heads
         scale = self.dim_head ** -0.5
 
         b, _, y, n = x.shape
         h = heads
-        q = DepthWiseConv2d(self.dim, inner_dim, self.proj_kernel, stride = (1, 1), bias = False)(x)
+        q = DepthWiseConv2d(self.dim, inner_dim, self.proj_kernel, stride = 1, bias = False)(x)
+
         kv = DepthWiseConv2d(self.dim, inner_dim * 2, self.proj_kernel, stride = self.kv_proj_stride, bias = False)(x)
         k, v = jnp.split(kv, 2, axis = -1)
         qkv = (q, k, v)
@@ -106,15 +103,15 @@ class Attention(nn.Module):
         x = einsum('b i j, b j d -> b i d', attn, v)
         x = rearrange(x, '(b h) (x y) d -> b x y (h d)', h = h, y=y)
 
-        x = nn.Conv(features = self.dim_out, kernel_size = (1, 1), strides = (1, 1), use_bias = False)(x)
+        x = nn.Conv(features = self.dim, kernel_size = (1, 1), strides = (1, 1), use_bias = False)(x)
         x = nn.Dropout(rate = self.dropout)(x, deterministic = False)
 
         return x
 
 class Transformer(nn.Module):
     dim: int 
-    proj_kernel, 
-    kv_proj_stride, 
+    proj_kernel: int 
+    kv_proj_stride: int 
     depth: int 
     heads: int 
     dim_head: int = 64 
@@ -122,14 +119,14 @@ class Transformer(nn.Module):
     dropout: float = 0.
 
     @nn.compact
-    def call(self, x):
+    def __call__(self, x):
 
         layers = []
 
         for _ in range(self.depth):
             layers.append([
-                PreNorm(self.dim, Attention(self.dim, proj_kernel = self.proj_kernel, kv_proj_stride = self.kv_proj_stride, heads = self.heads, dim_head = self.dim_head, dropout = self.dropout)),
-                PreNorm(self.dim, FeedForward(self.dim, self.mlp_mult, dropout = self.dropout))
+                PreNorm(Attention(self.dim, proj_kernel = self.proj_kernel, kv_proj_stride = self.kv_proj_stride, heads = self.heads, dim_head = self.dim_head, dropout = self.dropout)),
+                PreNorm(FeedForward(self.dim, self.mlp_mult, dropout = self.dropout))
             ])
 
         for i, (attn, ff) in enumerate(layers):
@@ -139,57 +136,95 @@ class Transformer(nn.Module):
         return x
 
 class CvT(nn.Module):
-    num_classes,
-    s1_emb_dim=64,
-    s1_emb_kernel=7,
-    s1_emb_stride=4,
-    s1_proj_kernel=3,
-    s1_kv_proj_stride=2,
-    s1_heads=1,
-    s1_depth=1,
-    s1_mlp_mult=4,
-    s2_emb_dim=192,
-    s2_emb_kernel=3,
-    s2_emb_stride=2,
-    s2_proj_kernel=3,
-    s2_kv_proj_stride=2,
-    s2_heads=3,
-    s2_depth=2,
-    s2_mlp_mult=4,
-    s3_emb_dim=384,
-    s3_emb_kernel=3,
-    s3_emb_stride=2,
-    s3_proj_kernel=3,
-    s3_kv_proj_stride=2,
-    s3_heads=6,
-    s3_depth=10,
-    s3_mlp_mult=4,
-    dropout=0.
+    num_classes: int
+    s1_emb_dim: int = 64
+    s1_emb_kernel: int = 7
+    s1_emb_stride: int = 4
+    s1_proj_kernel: int = 3
+    s1_kv_proj_stride: int = 2
+    s1_heads: int = 1
+    s1_depth: int = 1
+    s1_mlp_mult: int = 4
+    s2_emb_dim: int = 192
+    s2_emb_kernel: int = 3
+    s2_emb_stride: int = 2
+    s2_proj_kernel: int = 3
+    s2_kv_proj_stride: int = 2
+    s2_heads: int = 3
+    s2_depth: int = 2
+    s2_mlp_mult: int = 4
+    s3_emb_dim: int = 384
+    s3_emb_kernel: int = 3
+    s3_emb_stride: int = 2
+    s3_proj_kernel: int = 3
+    s3_kv_proj_stride: int = 2
+    s3_heads: int = 6
+    s3_depth: int = 10
+    s3_mlp_mult: int = 4
+    dropout: float = 0.
 
     @nn.compact
-    def __call__(self, img, training=True, **kwargs):
+    def __call__(self, x):
 
-        kwargs = dict(locals())
+        x = nn.Conv(
+            features = self.s1_emb_dim, 
+            kernel_size = (self.s1_emb_kernel, self.s1_emb_kernel),
+            padding = 'SAME', 
+            strides = (self.s1_emb_stride, self.s1_emb_stride),
+        )(x)
+        
+        x = LayerNorm(self.s1_emb_dim)(x)
+        
+        x = Transformer(
+            dim = self.s1_emb_dim,
+            proj_kernel = self.s1_proj_kernel,
+            kv_proj_stride = self.s1_kv_proj_stride,
+            depth = self.s1_depth,
+            heads = self.s1_heads,
+            mlp_mult = self.s1_mlp_mult,
+            dropout = self.dropout
+        )(x)
 
-        cvt_layers = nn.Sequential()
+        x = nn.Conv(
+            features = self.s2_emb_dim, 
+            kernel_size = (self.s2_emb_kernel, self.s2_emb_kernel), 
+            padding = 'SAME', 
+            strides = (self.s2_emb_stride, self.s2_emb_stride) 
+        )(x)
+        
+        x = LayerNorm(self.s2_emb_dim)(x)
 
-        for prefix in ('s1', 's2', 's3'):
-            config, kwargs = group_by_key_prefix_and_remove_prefix(f'{prefix}_', kwargs)
-            cvt_layers.add(nn.Sequential([
-                nn.Conv2D(filters=config['emb_dim'], kernel_size=config['emb_kernel'], padding='SAME', strides=config['emb_stride']),
-                LayerNorm(config['emb_dim']),
-                Transformer(dim=config['emb_dim'], proj_kernel=config['proj_kernel'],
-                            kv_proj_stride=config['kv_proj_stride'], depth=config['depth'], heads=config['heads'],
-                            mlp_mult=config['mlp_mult'], dropout = self.dropout)
-            ]))
+        x = Transformer(
+            dim=self.s2_emb_dim, 
+            proj_kernel=self.s2_proj_kernel, 
+            kv_proj_stride=self.s2_kv_proj_stride, 
+            depth=self.s2_depth, 
+            heads=self.s2_heads, 
+            mlp_mult=self.s2_mlp_mult, 
+            dropout=self.dropout
+        )(x)
 
+        x = nn.Conv(
+            features = self.s3_emb_dim, 
+            kernel_size = (self.s3_emb_kernel, self.s3_emb_kernel), 
+            padding = 'SAME', 
+            strides = (self.s3_emb_stride, self.s3_emb_stride)
+        )(x)
+        
+        x = LayerNorm(self.s3_emb_dim)(x)
 
-        cvt_layers.add(nn.Sequential([
-            nn.GlobalAvgPool2D(),
-            nn.Dense(units = num_classes)
-       ]))
+        x = Transformer(
+            dim=self.s3_emb_dim, 
+            proj_kernel=self.s3_proj_kernel, 
+            kv_proj_stride=self.s3_kv_proj_stride, 
+            depth=self.s3_depth, 
+            heads=self.s3_heads, 
+            mlp_mult=self.s3_mlp_mult, 
+            dropout=self.dropout
+        )(x)
 
-        x = cvt_layers(img)
+        x = GlobalAvgPool()(x)
+        x = nn.Dense(features = self.num_classes)(x)
         return x
 
 
@@ -235,8 +270,7 @@ if __name__ == '__main__':
                 'emb_dropout': jax.random.PRNGKey(3)}
 
     params = v.init(init_rngs, img)
-    output = v.apply(params, img, rngs=init_rngs)
-    print(output.shape)
+    output = v.apply(params, img, mutable=['batch_stats'], rngs=init_rngs)
 
     n_params_flax = sum(
         jax.tree_leaves(jax.tree_map(lambda x: numpy.prod(x.shape), params))
